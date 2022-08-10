668798f0509fa2a06e084581f75fc79030657f27
Shivams9 <91686685+Shivams9@users.noreply.github.com>
2022-08-10 18:24:04 +0530

    s

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("test/", hello.views.test,  name = "test"),
     path("medicine/", hello.views.medicine,  name = "medicine"),
+    path("search/",hello.views.search, ),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+
+from django.shortcuts import render,HttpResponse
 
 from .models import Greeting
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+
 def test(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "test.html")
+
 
 def medicine(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "medicine.html")
 
-def db(request):
 
+def search(request):
+    try:
+        rollno = request.GET["rollno"]
+        d = {"status": "ok", "rollno": rollno, "result": "pasword"}
+        d= json.dumps(d)
+        return HttpResponse(d)
+    except:
+        d={"status":"error"}
+        d= json.dumps(d)
+        return HttpResponse(d)
+def db(request):
     greeting = Greeting()
     greeting.save()
 
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
